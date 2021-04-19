6945901a20d408eb7f3f2e123da34f591b32fafa
snyt45 <snyt45@gmail.com>
2021-04-19 22:25:17 +0900

    add env.zsh xfce4

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -9,3 +9,6 @@ export PATH=/home/linuxbrew/.linuxbrew/sbin:$PATH
 # Go
 export GOPATH=$HOME
 export PATH=$GOPATH/bin:$PATH
+
+# xfce4
+export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
